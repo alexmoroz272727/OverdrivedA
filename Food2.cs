c0bf6d942ed96ccf614980dff4e615bea64a5f77
Perenval <alexmoroz272727@mail.ru>
2019-03-02 23:32:18 +0300

    StableSpawn

---
+++ Food2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Food2 : MonoBehaviour
+{
+    
+
+   void zxcv()
+    {
+       GameObject.Find("GameHelper1").GetComponent<FoodGen>().eat1 = false; 
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.CompareTag("waste"))
+        {
+
+            zxcv();
+
+
+
+            Destroy(gameObject);
+        }
+
+
+        
+            if (other.CompareTag("SnakeMain"))
+            {
+                other.GetComponent<SnakeMovment>().AddTail1();
+                
+                zxcv();
+
+
+
+                Destroy(gameObject);
+            }
+        
+
+    }
+}
